de56363d5f96928eb305434296d3debcfe73ae44
Bremdow <dbsalazarr@gmail.com>
2023-07-25 10:45:38 -0600

    Probando el decode para el código gray

--- binary_decode.py
+++ binary_decode.py
@@ -1,3 +1,6 @@
+import decimal_to_binary as binary
+import binary_encode as gray
+
 def gray_decode( gray_code ) :
     """
         Esta es una función para decodificar el código gray generado
@@ -14,15 +17,34 @@ def gray_decode( gray_code ) :
             desarrollo 
     """
     binary_number = gray_code[0]
-    for i in range( len(gray_code) ) :
-        sum = gray_code[i] + gray_code[i + 1]
+    for i in range( len(gray_code) - 1 ) :
+        sum = int(gray_code[i]) + int(gray_code[i + 1])
         if sum >= 2 :
             binary_number += "0"
         else :
-            
-    
+            binary_number += str( int(gray_code[i]) + int(gray_code[i + 1]) )
+    return binary_number
+
 def main():
-    pass
+    binary_numbers = []
+    numbers_gray = []
+    binary_decode = []
+    for i in range(0, 16) :
+        number_binary = binary.convert_n_bits(4, binary.decimal_to_binary(i) )
+        binary_numbers.append( number_binary)
+        
+        number_gray = gray.gray_encode(number_binary)
+        numbers_gray.append( number_gray )
+        
+        binary_decode.append( gray_decode( number_gray) )
+        
+        
+    print( "BINARY NUMBERS" )
+    print( binary_numbers)
+    print( "GRAY CODE" )
+    print( numbers_gray )
+    print( " BINARY NUMBER " )
+    print( binary_decode )    
 
 if __name__ == "__main__" :
     main()
