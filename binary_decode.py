e31d651a76cf80bf972a403733586291631e4a76
bremdows <bremdows@gmail.com>
2023-07-30 14:11:54 -0500

    Codificacion gray - binario terminada

--- binary_decode.py
+++ binary_decode.py
@@ -10,27 +10,39 @@ def gray_decode( gray_code ) :
             str : Retorna el código gray decodificado (número binario original)
 
         Ejemplo :
+            Gray => Binario
+
             0001 => 0001
             0011 => 0010
             0010 => 0011
+            
+            Gray: 0 1 1 1
+            Bina: 0 1 0 1
+        ANALISIS DEL PROBLEMA
+        Se tiene lo siguiente
+        Primer bit : es el MSB 
+        Segundo bit: Suma del MSB + el 2do bit
+        Tercer bit : (Suma del MSB + el 2do bit) + 3er bit
+        Cuarto bit : (Suma del MSB + el 2do bit + 3er bit) + 4to bit
 
-            0 1 1 1
-            0 1 0 1
     """
     binary_number = gray_code[0]
+    aux = gray_code[0]
     for i in range( len(gray_code) - 1) :
-        binary_number[i+1] += binary_number[i] + gray_code[i+1]
-
+        if int(aux) + int(gray_code[i+1]) >= 2 :
+            binary_number += "0"
+            aux = "0"
+        else :
+            binary_number += str( int(aux) + int(gray_code[i+1]) )
+            aux = str( int(aux) + int(gray_code[i+1]))
     return binary_number
-            
-
 
 def main():
     binary_numbers = []
     numbers_gray = []
     binary_decode = []
-    for i in range(0, 16) :
-        number_binary = binary.convert_n_bits(4, binary.decimal_to_binary(i) )
+    for i in range(0, 32) :
+        number_binary = binary.convert_n_bits(5, binary.decimal_to_binary(i) )
         binary_numbers.append( number_binary)
         
         number_gray = gray.gray_encode( number_binary )
@@ -44,7 +56,12 @@ def main():
     print( "GRAY CODE" )
     print( numbers_gray )
     print( " BINARY NUMBER " )
-    print( binary_decode )    
+    print( binary_decode ) 
+
+    print( "OTHERS" ) 
+    print( binary_numbers[5] )
+    print( numbers_gray[5] )
+    print( binary_decode[5] )
 
 if __name__ == "__main__" :
     main()
